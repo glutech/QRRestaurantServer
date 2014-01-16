40648c72806b658f0791d22bda34fe6b91dbad38
Jokinryou Tsui <jokinryou.tsui@gmail.com>
2014-01-16 11:28:36 +0800

    add some file to make build correct

---
+++ src/main/resources/Keep.md
@@ -0,0 +1,1 @@
+###This file is used to keep this folder exist in git
---
+++ src/test/java/Keep.java
@@ -0,0 +1,9 @@
+
+/**
+ * This file is used to keep this folder exist in git
+ * @author jokinryou
+ *
+ */
+public class Keep {
+
+}
